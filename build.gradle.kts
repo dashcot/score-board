f1bbd4ab53abeeb5d651f5a60b2592b42ef2630e
dashcot <daschot.xc@gmail.com>
2023-07-30 18:49:19 +0300

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform") version "1.8.10"
+    id("maven-publish")
+}
+
+group = "com.sportradar.scoreboard"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    sourceSets {
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
